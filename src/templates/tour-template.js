54e4ee8c0e1b82e8ee1f74c39055a764835face5
John <johnsmilga@gmail.com>
2019-06-29 00:00:44 -0700

    Layout changed in template

--- src/templates/tour-template.js
+++ src/templates/tour-template.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { graphql } from 'gatsby'
-import Layout from '../components/Layout'
+import Layout from '../components/layout'
 import styles from '../css/template.module.css'
 import Img from 'gatsby-image'
 import { FaMoneyBillWave, FaMap } from 'react-icons/fa'
