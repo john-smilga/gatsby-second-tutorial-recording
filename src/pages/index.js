087eb1209e40efbc17ddf3ee9b8e56ef09ca2e2c
John <johnsmilga@gmail.com>
2019-06-28 23:55:50 -0700

    some errors fixed

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Layout from '../components/Layout'
+import Layout from '../components/layout'
 import { Button } from '../components/button'
 export default () => (
   <Layout>
